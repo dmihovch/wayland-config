6e2f9078f28755eee7883485efc49197d3ea24a4
Daniel Mihovch <dmihovch@gmail.com>
2026-02-26 19:43:26 -0500

    docker, go, etc

--- sys-setup.sh
+++ sys-setup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-sudo apt update && sudo apt install sway waybar swayidle swaylock wlsunset dex policykit-1-gnome network-manager-gnome blueman pulseaudio-utils brightnessctl foot wofi grim slurp wl-clipboard pavucontrol git curl xdg-desktop-portal-wlr 
+sudo apt update && sudo apt install sway waybar swayidle swaylock wlsunset dex policykit-1-gnome network-manager-gnome blueman pulseaudio-utils brightnessctl foot wofi grim slurp wl-clipboard pavucontrol git curl xdg-desktop-portal-wlr clang
 
 sudo snap install nvim --classic
+sudo snap install go --classic
+sudo snap install docker 
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+
 
 wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/UbuntuMono.zip
 mkdir -p ~/.local/share/fonts
