159e1957fb6844854f2898a7f6f91618659269aa
Daniel Mihovch <dmihovch@gmail.com>
2026-03-08 01:04:04 -0500

    confg

--- sys-setup.sh
+++ sys-setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo apt update && sudo apt install sway waybar swayidle swaylock wlsunset dex policykit-1-gnome network-manager-gnome blueman pulseaudio-utils brightnessctl foot wofi grim slurp wl-clipboard pavucontrol git curl xdg-desktop-portal-wlr clang
+sudo apt update && sudo apt install sway waybar swayidle swaylock wlsunset dex policykit-1-gnome network-manager-gnome blueman pulseaudio-utils brightnessctl foot wofi grim slurp wl-clipboard pavucontrol git curl xdg-desktop-portal-wlr clang clang-format python3.12-venv
 
 sudo snap install nvim --classic
 sudo snap install go --classic
